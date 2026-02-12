fcacf1e514060b03f31b04360cf6786e59b9a818
scrapychain <mindquaker20@gmail.com>
2026-02-12 00:20:01 -0500

    added test for empty string

--- src/hash.rs
+++ src/hash.rs
@@ -18,4 +18,12 @@ mod tests {
             "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
         );
     }
+    #[test]
+    fn test_empty() {
+        let result = hash("");
+        assert_eq!(
+            result,
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        );
+    }
 }
