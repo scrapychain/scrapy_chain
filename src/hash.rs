74cd34a03131b40740d619c40a7d643d43846c75
scrapychain <mindquaker20@gmail.com>
2026-02-11 23:48:27 -0500

    Added unit test for hash function

--- src/hash.rs
+++ src/hash.rs
@@ -5,3 +5,17 @@ pub fn hash(data: &str) -> String {
     hasher.update(data);
     format!("{:x}", hasher.finalize())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hash_hello() {
+        let result = hash("hello");
+        assert_eq!(
+            result,
+            "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+        );
+    }
+}
